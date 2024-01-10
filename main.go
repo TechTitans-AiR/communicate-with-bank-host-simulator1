b7e522433de9dc9af88c6352f4312b2753e76c91
GrgoPenava <grgopenava00@gmail.com>
2024-01-10 17:56:48 +0100

    Add endpoint to communicate with bank host simulator

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/sendToBankHostSimulator", handleTransaction)
+	http.ListenAndServe(":8084", nil)
+}
+
+type RequestBody struct {
+	CardNumber     string  `json:"cardNumber"`
+	ExpirationDate string  `json:"expirationDate"`
+	Balance        float64 `json:"balance"`
+	CVC            int     `json:"cvc"`
+}
+
+func handleTransaction(w http.ResponseWriter, r *http.Request) {
+	var requestBody RequestBody
+	err := json.NewDecoder(r.Body).Decode(&requestBody)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	apiURL := "http://localhost:8083/api/v1/transactions/processTransaction"
+	jsonBody, err := json.Marshal(requestBody)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	resp, err := http.Post(apiURL, "application/json", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(resp.StatusCode)
+	w.Write(responseBody)
+}
